b5732ba1caa82a203326f46d80a4fec6846f2251
unknown <nawfalahmed204@gmail.com>
2026-05-07 21:25:09 +0530

    add createsu management command

--- stockpredictor/stockpredictor/settings.py
+++ stockpredictor/stockpredictor/settings.py
@@ -97,4 +97,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-AUTH_USER_MODEL = 'adminpanel.AdminUser'
+# AUTH_USER_MODEL = 'adminpanel.AdminUser'
